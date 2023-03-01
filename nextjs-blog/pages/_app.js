be171f029a876cc9ec1792fc1128f2021552f24a
Jidoex <pierre.anxionnat@depinfonancy.net>
2023-03-01 11:27:58 +0100

    Other trainings done

---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,7 @@
+/**Allows importing CSS styles globally */
+
+import '../styles/global.css';
+
+export default function App({ Component, pageProps}) {
+    return <Component {...pageProps}/>;
+}
---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi ! I'm Jidoex (aka Pierre) 
+          <br/> I'm a software engineering student in the Mines de Nancy (France) engineering school.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
